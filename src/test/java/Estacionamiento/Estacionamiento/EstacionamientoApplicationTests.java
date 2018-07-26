b753b02ca7ae58bcf2fa9ba11e1f1a775efcb20a
Yeason10 <yeason.ortiz@ceiba.com.co>
2018-07-26 10:53:06 -0500

    7/26/2018

--- src/test/java/Estacionamiento/Estacionamiento/EstacionamientoApplicationTests.java
+++ src/test/java/Estacionamiento/Estacionamiento/EstacionamientoApplicationTests.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EstacionamientoApplicationTests {
 
+	@Autowired
+	public EstacionamientoApplicationTests(){}
+	
 	@Test
 	public void contextLoads() {
 		
